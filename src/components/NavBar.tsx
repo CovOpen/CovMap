13024a58aa0f1ce8f2f0ebb441f7970e13e967ee
Theresa Tobollik <theresa.joana.tobollik@uni-weimar.de>
2020-04-09 13:23:14 +0200

    adding search bar - not functional yet

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,15 +8,23 @@ import Menu from '@material-ui/core/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import { AppApi, Step } from "state/app";
 import { useThunkDispatch } from "useThunkDispatch";
+import SearchIcon from '@material-ui/icons/Search';
+import { fade} from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
 
 import { AnimatedLogo } from "./AnimatedLogo";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   menuButton: {
+    paddingRight: 10,
     marginRight: theme.spacing(2),
+  },
+  title: {
+    flexShrink: 1
   },
   menuItem: {
     touchAction: 'none'
@@ -26,6 +34,47 @@ const useStyles = makeStyles((theme) => ({
   },
   menuContent: {
     backgroundColor: theme.palette.primary.light
+  },
+  logo: {
+    height: '32px', 
+    width: 'auto', 
+    marginTop: '9px'
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.85),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.95),
+    },
+    marginLeft: theme.spacing(2),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
   },
 }));
 
@@ -48,6 +97,24 @@ export const NavBar = () => {
     <AppBar position="static" style={{ position: 'relative', zIndex: 1200, touchAction: 'none' }}>
       <Toolbar style={{ height: 64 }}>
         <AnimatedLogo />
+        <Typography variant="h6" className={classes.title}>
+          <img src="/logo.svg" className={classes.logo} />
+        </Typography>
+
+        <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="PLZ oder Wohnort..."
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+        </div>
+
         <div>
           <IconButton
             aria-label="account of current user"
@@ -58,6 +125,7 @@ export const NavBar = () => {
           >
             <MenuIcon />
           </IconButton>
+          
           <Menu
             className={classes.menu}
             id="menu-appbar"
