032eeb9e4535a3a7f5c9b2bdcbd3d1c934cfa6f6
Christian Romberg <distjubo@gmail.com>
2020-04-10 16:11:37 +0200

    Adapt data generator to generate for districts Example call: ts-node dev/generate-random-data-for-geojson.js -p "name_2" data/districts_small.geojson

--- dev/generate-random-data-for-geojson.js
+++ dev/generate-random-data-for-geojson.js
@@ -8,6 +8,7 @@ program
   .name("generate-random-data-for-geojson")
   .option('-d, --days <amount>', 'Generate the given amount of daily random data')
   .option('-o, --output <path>', 'Where to output the generated JSON relative to CWD (default: next to input file)')
+  .option('-p, --property <name>', 'Which property is used for identifying a feature (default: PLZ99)')
   .arguments('<cmd> [geojsonfile]');
 
 program.parse(process.argv);
@@ -30,20 +31,25 @@ if (program.output) {
   output = path.join(pathInfo.dir, `${pathInfo.name}.generated.json`);
 }
 
+let propertyName = 'PLZ99'
+if(program.property) {
+  propertyName = program.property
+}
+
 if (program.days) {
   const numDays = parseInt(program.days)
   if (!numDays) {
     throw new Error('Days need to be a positive integer')
   }
   const pathInfo = path.parse(geojsonPath);
-  
+
   for (let day = 0; day < numDays; day += 1) {
     const dayOutput = path.join(pathInfo.dir, `${pathInfo.name}.generated.${formatNowMinusDays(day)}.json`);
-    const randomData = randomDataForJson(geojson)
+    const randomData = randomDataForJson(geojson, propertyName)
     writeFileSync(dayOutput, JSON.stringify(randomData));
   }
 } else {
-  const randomData = randomDataForJson(geojson)
+  const randomData = randomDataForJson(geojson, propertyName)
   writeFileSync(output, JSON.stringify(randomData));
 }
 
@@ -53,12 +59,12 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomDataForJson(json) {
+function randomDataForJson(json, propertyName) {
   return json.features.reduce((acc, data) => {
     Object.assign(acc.data, {
-      [data.properties.PLZ99]: { coughs: getRandomInt(0, 100) }
+      [data.properties[propertyName]]: { coughs: getRandomInt(0, 100) }
     });
-  
+
     return acc;
   }, { types: { coughs: "Coughing Symptoms" }, data: {} })
 }
