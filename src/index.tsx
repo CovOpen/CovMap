76ed5743cadee39550825d6a165e66793a84c850
Sebastian Herrlinger <hasta84@gmail.com>
2020-04-20 02:35:40 +0200

    remove unused code

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,8 @@ import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 import { store } from "./state";
-import { AppApi } from "./state/app";
 
 const root = document.getElementById("app");
-
-
-// window.addEventListener("popstate", (e: PopStateEvent) => {
-//     store.dispatch(AppApi.gotoPage(Page.Map))
-// })
-
-store.subscribe(() => {
-  (window as any).state = store.getState();
-});
-(window as any).go = (pageId: string) => store.dispatch(AppApi.gotoPage(pageId))
-
 const persistor = persistStore(store);
 
 render(
