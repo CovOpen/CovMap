b344dc66278208fb92caf4f19432c5aa3caf4429
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-08-24 20:24:31 +0200

    Update About.tsx

--- apps/official/components/pages/About.tsx
+++ apps/official/components/pages/About.tsx
@@ -8,22 +8,28 @@ export const About = () => {
     <>
       <main className="sections">
         <section>
-          <Typography variant="h1">About CovMap</Typography>
+          <Typography variant="h1">About</Typography>
         </section>
         <section>
-          <Typography variant="h2">About this Mapper app...</Typography>
+          <Typography variant="h2">Was ist CovMap?</Typography>
           <Typography variant="body1">
-            Some Paragraph
+            Unser momentanes Kontaktverhalten ist ein wesentlicher Grund, warum sich das Coronavirus (SARS-CoV-2) nur langsam in Deutschland ausbreiten kann. Das Ziel von CovMap ist es, an den Umstand zu erinnern, dass jeder Bürger dabei helfen kann, Infektionen durch Kontaktbeschränkugen zu vermeiden. Auf einer Deutschlandkarte wird pro Landkreis das momentane Kontaktverhalten der Bevölkerung dargestellt.
           </Typography>
           <Typography variant="body1">
-            yes yes, another paragraph
+            Ein weiteres Ziel von CovMap ist es, lokale Ausbrüche des Virus möglichst frühzeitig zu detektieren.
           </Typography>
         </section>
-        <section>
-          <Typography variant="h2">Was passiert mit den Daten?</Typography>
+          <section>
+          <Typography variant="h2">Wie funktioniert CovMap?</Typography>
           <Typography variant="body1">
-            All deine Daten werden nur anonymisiert weiterverarbeitet.
-            Wir erheben keine Bewegungsdaten.
+            CovMap wertet Daten zu den frühestmöglichen Etappen der Viruserkrankung aus: dem Kontakt zwischen Menschen, bei dem das Virus übertragen werden kann, und dem Auftreten von Symptomen. Hierzu werten wir GPS- und selbstberichtete Symptome der Bevölkerung anonym aus.
+          </Typography>
+        </section>
+
+        <section>
+          <Typography variant="h2">Kann ich CovMap nutzen, ohne Daten preiszugeben?</Typography>
+          <Typography variant="body1">
+            Die Erhebung Deiner Daten ist vollkommen freiwillig und Du kannst die CovMap Karte auch nutzen, ohne dass Du uns Deine Daten bereitstellst. Bitte bedenke, dass unser Dienst jedoch nur funktionieren kann, wenn ausreichend viele Menschen aktiv daran teilnehmen.
           </Typography>
           <div className="btn-group">
             <Button
