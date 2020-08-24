2fade33f99c9e126606d9d560b381379b640aca0
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-08-24 22:48:49 +0200

    Update Imprint.tsx
    Update

--- apps/official/components/pages/Imprint.tsx
+++ apps/official/components/pages/Imprint.tsx
@@ -9,7 +9,7 @@ export const Imprint = () => {
           <Typography variant="h1">Impressum</Typography>
         </section>
         <section>
-          <Typography variant="h2">Die CovMap App ist ein Forschungsprojekt von Mitarbeiten der Charité, NETCHECK und dem Hasso Plattner Institut.</Typography>
+          <Typography variant="h2">Die CovMap App ist ein Forschungsprojekt von Mitarbeiten der Charité, der NETCHECK GmbH und des Hasso Plattner Instituts.</Typography>
           <Typography variant="body1">Falls Du Fragen hast, wende Dich an:</Typography>
           <Typography variant="body1">CovMap Team, Augustenburger Platz 1, 13353 Berlin, Telefon: 030/450627346, Email: covmap@charite.de</Typography>
         </section>
