12c96b9f9b16b2b28c682572b96013105996d571
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-08-24 22:11:37 +0200

    Update About.tsx
    Update

--- apps/official/components/pages/About.tsx
+++ apps/official/components/pages/About.tsx
@@ -13,23 +13,23 @@ export const About = () => {
         <section>
           <Typography variant="h2">Was ist CovMap?</Typography>
           <Typography variant="body1">
-            Unser momentanes Kontaktverhalten ist ein wesentlicher Grund, warum sich das Coronavirus (SARS-CoV-2) nur langsam in Deutschland ausbreiten kann. Das Ziel von CovMap ist es, an den Umstand zu erinnern, dass jeder Bürger dabei helfen kann, Infektionen durch Kontaktbeschränkugen zu vermeiden. Auf einer Deutschlandkarte wird pro Landkreis das momentane Kontaktverhalten der Bevölkerung dargestellt.
+            Dad momentane Kontaktverhalten ist ein wesentlicher Grund dafür, weshalb sich das Coronavirus (SARS-CoV-2) nur langsam in Deutschland ausbreiten kann. Das Ziel von CovMap ist es, an den Umstand zu erinnern, dass jeder Bürger dabei helfen kann, Infektionen durch das eigene Verhalten zu vermeiden. Auf einer Deutschlandkarte stellen wir das momentane Kontaktverhalten pro Landkreis dar und bewerten es hinsichtlich des Risikos von neuen Infektionen.
           </Typography>
           <Typography variant="body1">
-            Ein weiteres Ziel von CovMap ist es, lokale Ausbrüche des Virus möglichst frühzeitig zu detektieren.
+            Ein weiteres Ziel von CovMap ist es, lokale Ausbrüche des Virus möglichst frühzeitig zu erkennen.
           </Typography>
         </section>
           <section>
           <Typography variant="h2">Wie funktioniert CovMap?</Typography>
           <Typography variant="body1">
-            CovMap wertet Daten zu den frühestmöglichen Etappen der Viruserkrankung aus: dem Kontakt zwischen Menschen, bei dem das Virus übertragen werden kann, und dem Auftreten von Symptomen. Hierzu werten wir GPS- und selbstberichtete Symptome der Bevölkerung anonym aus.
+            CovMap wertet die frühestmöglichen Etappen eine Viruserkrankung aus: dem Kontakt, bei dem das Virus übertragen wird, und dem Auftreten von Symptomen. Hierzu werten wir große Datenmengen an GPS- und Symptomdaten der Bevölkerung aus.
           </Typography>
         </section>
 
         <section>
           <Typography variant="h2">Kann ich CovMap nutzen, ohne Daten preiszugeben?</Typography>
           <Typography variant="body1">
-            Die Erhebung Deiner Daten ist vollkommen freiwillig und Du kannst die CovMap Karte auch nutzen, ohne dass Du uns Deine Daten bereitstellst. Bitte bedenke, dass unser Dienst jedoch nur funktionieren kann, wenn ausreichend viele Menschen aktiv daran teilnehmen.
+            Die Erhebung Deiner Daten ist vollkommen freiwillig und Du kannst die Karte von CovMap auch nutzen, ohne dass Du uns Deine Daten bereitstellst. Bitte bedenke, dass unser Dienst jedoch nur funktionieren kann, wenn ausreichend viele Menschen aktiv daran teilnehmen.
           </Typography>
           <div className="btn-group">
             <Button
