43280c91beb4221e07cd2f226e4dd6a1a92779e5
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-08-26 01:18:31 +0200

    Update CovMapWelcome.tsx

--- apps/official/components/CovMapWelcome.tsx
+++ apps/official/components/CovMapWelcome.tsx
@@ -9,8 +9,11 @@ export const CovMapWelcome = () => {
       <DialogTitle id="alert-dialog-title">{"Willkommen bei CovMap "}</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-            CovMap ist ein Gemeinschaftsprojekt der Charité, NETCHECK GmbH und des Hasso Plattner Instituts.
-            Ein vermehrtes Kontaktverhalten geht mit einem erhöhten Risiko einer Infektion mit dem Coronavirus einher. Im Folgenden zeigen wir Dir eine Karte, aus der Du das aktuelle Kontaktverhalten der Bevölkerung pro Landkreis ersehen kannst. Je intensiver der Blauton in Deinem Landkreis ist, umso mehr Kontakte liegen vor. Falls in Deinem Landkreis ein erhöhtes Kontaktverhalten vorliegt, dann überdenke, ob Du Deine Kontakte einschränken kannst! Jeder Bürger kann dabei helfen, die Ausbreitung des Coronavirus zu verlangsamen!
+Demoversion für interne Demonstrationszwecke.   
+         CovMap ist ein Gemeinschaftsprojekt der Charité, der NETCHECK GmbH und des Hasso Plattner Instituts.
+            Ein vermehrtes Kontaktverhalten kann mit einem erhöhten Risiko von neuen Infektion mit dem Coronavirus einhergehen. Im Folgenden zeigen wir Dir eine Karte, aus der Du das aktuelle Kontaktverhalten der Bevölkerung pro Landkreis ersehen kannst. Je intensiver der Blauton in Deinem Landkreis ist, umso mehr Kontakte liegen vor. Falls in Deinem Landkreis ein erhöhtes Kontaktverhalten vorliegen sollte, dann überdenke, ob Du Deine Kontakte einschränken kannst! Jeder Bürger kann dabei helfen, die Ausbreitung des Coronavirus zu verlangsamen!
+
+
         </DialogContentText>
       </DialogContent>
     </>
