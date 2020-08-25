c51218b1ddbfb2d76f827ac03471842285f09325
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-08-25 22:16:40 +0200

    Update CovMapWelcome.tsx

--- apps/official/components/CovMapWelcome.tsx
+++ apps/official/components/CovMapWelcome.tsx
@@ -9,9 +9,8 @@ export const CovMapWelcome = () => {
       <DialogTitle id="alert-dialog-title">{"Willkommen bei CovMap "}</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-            Hier siehst du die aktuelle Anzahl an Corona Infizierten pro Landkreis in Deutschland.
-            Du kannst dir auch andere Daten anschauen, indem du auf das Menü unten links klickst. Oder schaue
-            dir den zeitlichen Verlauf an, indem du die Zeitleiste am unteren Bildschirmrand bewegst.
+            CovMap ist ein Gemeinschaftsprojekt der Charité, NETCHECK GmbH und des Hasso Plattner Instituts.
+            Ein vemehrtes Kontaktverhalten geht mit einem erhöhten Risiko einer Infektion mit dem Coronavirus einher. Im Folgenden zeigen wir Dir eine Karte, aus der Du das aktuelle Kontaktverhalten der Bevölkerung pro Landkreis ersehen kannst. Je intensiver der Blauton in Deinem Landkreis ist, umso mehr Kontakte liegen vor. Falls dein Landkreis dunkelblau ist, überdenke, ob Du deine Kontakte einschränken kannst! Jeder Bürger kann dabei helfen, die Ausbreitung des Coronavirus zu verlangsamen!
         </DialogContentText>
       </DialogContent>
     </>
