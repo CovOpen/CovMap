e4b4f41fac16186831968c4c7cfaa1854555a519
Christian Romberg <distjubo@gmail.com>
2020-09-08 14:51:55 +0200

    Load charts data from JSON

--- apps/official/components/pages/Charts.tsx
+++ apps/official/components/pages/Charts.tsx
@@ -3,47 +3,35 @@ import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
 } from 'recharts';
 
-export const Charts = () => {
+import chartsdata from "../../static/ChartsData.json"
 
-  const data = [
-    {
-      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
-  ];
+export const Charts = () => {
+  const links = [2294, 2302, 2311, 2341]
+  const index = Math.floor(Math.random() * links.length)
+  const link = links[index]
+  
+  const indices = Array(chartsdata.days.length).fill(undefined).map((_, index) => index)
+  
   return (
+  <a href={`https://xkcd.com/${link}/`}>
   	<ResponsiveContainer>
       <LineChart
-        data={data}
+        data={indices}
         margin={{
           top: 20, right: 30, left: 20, bottom: 10,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
+        <XAxis dataKey={(x) => chartsdata.days[x]}/>
+        <YAxis yAxisId="left" />
+        <YAxis yAxisId="right" orientation="right" />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        <Line yAxisId="left" type="monotone" dataKey={(x) => chartsdata.ci[x]} stroke="#8884d8" name="Contact Index C"/>
+        <Line yAxisId="left" type="monotone" dataKey={(x) => chartsdata.md[x]} stroke="#82ca9d" name="Mean Contacts"/>
+        <Line yAxisId="right" type="monotone" dataKey={(x) => chartsdata.R[x]} stroke="#f01a8d" name="Reproduction rate R"/>
       </LineChart>
     </ResponsiveContainer>
+  </a>
   );
 };
