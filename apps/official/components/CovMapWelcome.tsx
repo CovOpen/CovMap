29339c35c99c2b4c216770e91c57488f6104b73f
Andreas Lubbe <git@lubbe.org>
2020-10-05 16:49:11 +0200

    Akkreditierung technische Unterstützung

--- apps/official/components/CovMapWelcome.tsx
+++ apps/official/components/CovMapWelcome.tsx
@@ -9,11 +9,9 @@ export const CovMapWelcome = () => {
       <DialogTitle id="alert-dialog-title">{"Willkommen bei CovMap "}</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-Demoversion für interne Demonstrationszwecke.   
-         CovMap ist ein Gemeinschaftsprojekt der Charité, der NETCHECK GmbH und des Hasso Plattner Instituts.
-            Ein vermehrtes Kontaktverhalten kann mit einem erhöhten Risiko von neuen Infektionen mit dem Coronavirus (SARS-CoV-2) einhergehen. Im Folgenden zeigen wir Dir eine Karte, aus der Du das aktuelle Kontaktverhalten der Bevölkerung pro Landkreis ersehen kannst. Je intensiver der Blauton in Deinem Landkreis ist, umso mehr Kontakte liegen vor. Falls in Deinem Landkreis ein erhöhtes Kontaktverhalten vorliegen sollte, dann überdenke, ob Du Deine Kontakte einschränken kannst! Jeder Bürger kann dabei helfen, die Ausbreitung des Coronavirus zu verlangsamen!
-
-
+          Demoversion für interne Demonstrationszwecke.   
+          CovMap ist ein Gemeinschaftsprojekt der Charité, der NETCHECK GmbH und des Hasso Plattner Instituts, mit technischer Unterstützung von Port Blue Sky sowie vielen voluntären Unterstützern.
+          Ein vermehrtes Kontaktverhalten kann mit einem erhöhten Risiko von neuen Infektionen mit dem Coronavirus (SARS-CoV-2) einhergehen. Im Folgenden zeigen wir Dir eine Karte, aus der Du das aktuelle Kontaktverhalten der Bevölkerung pro Landkreis ersehen kannst. Je intensiver der Blauton in Deinem Landkreis ist, umso mehr Kontakte liegen vor. Falls in Deinem Landkreis ein erhöhtes Kontaktverhalten vorliegen sollte, dann überdenke, ob Du Deine Kontakte einschränken kannst! Jeder Bürger kann dabei helfen, die Ausbreitung des Coronavirus zu verlangsamen!
         </DialogContentText>
       </DialogContent>
     </>
