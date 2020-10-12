d66f0462a451fbb78a45ec67be6bcd10ebf85e2b
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-10-12 19:21:32 +0200

    Update Imprint.tsx

--- apps/official/components/pages/Imprint.tsx
+++ apps/official/components/pages/Imprint.tsx
@@ -10,14 +10,54 @@ export const Imprint = () => {
         </section>
         <section>
           <Typography variant="h2">
-            CovMap ist ein Gemeinschaftsprojekt der Charité, der NETCHECK GmbH und des Hasso Plattner Instituts.
+            Anbieter
           </Typography>
-          <Typography variant="body1">Falls Du Fragen hast, wende Dich an:</Typography>
-          <Typography variant="body1">
-            CovMap Team, Augustenburger Platz 1, 13353 Berlin, Telefon: 030/450627346, Email:
-            alexander-henry.thieme@charite.de
-          </Typography>
+          <Typography variant="body1">Charité – Universitätsmedizin Berlin</Typography>
+          <Typography variant="body1">Zentrale Postanschrift: Charitéplatz 1, 10117 Berlin</Typography>
+          <Typography variant="body1">Die Charité – Universitätsmedizin Berlin ist eine Körperschaft des Öffentlichen Rechts. Sie wird durch den Vorstandsvorsitzenden gesetzlich vertreten.
+</Typography>
         </section>
+                <section>
+          <Typography variant="h2">
+            Kontakt
+          </Typography>
+          <Typography variant="body1">Für Hinweise, Lob und Kritik schreiben Sie uns bitte eine E-Mail an: covmap@charite.de
+</Typography>
+          <Typography variant="body1">Internet: https://www.charite.de</Typography>
+        </section>
+        
+        
+                <section>
+          <Typography variant="h2">
+            Verantwortlicher im Sinne des Medienrechts
+          </Typography>
+          <Typography variant="body1">Prof. Dr. Heyo K. Kroemer, der Vorstandsvorsitzende der Charité – Universitätsmedizin Berlin
+</Typography>
+          <Typography variant="body1"></Typography>                            
+          <Typography variant="body1">Verantwortlich für Inhalte:
+</Typography>
+          <Typography variant="body1">Projektleitung: Dr. med. Alexander H. Thieme, M.Sc.</Typography>
+          <Typography variant="body1">Head of Product: Dr. med. Mirja Mittermaier</Typography>
+                  
+<Typography variant="body1">Verantwortlich Software Entwicklung:</Typography>
+<Typography variant="body1">Head Charité: Dr. med. Alexander H. Thieme, M.Sc.</Typography>
+        </section>
+        
+        
+                        <section>
+          <Typography variant="h2">
+            Zuständige Aufsichtsbehörde
+          </Typography>
+          <Typography variant="body1">Der Regierende Bürgermeister von Berlin – inkl. Wissenschaft und Forschung 
+</Typography>
+          <Typography variant="body1">Kontakt: <a href="https://www.berlin.de/rbmskzl/">https://www.berlin.de/rbmskzl/</a>
+</Typography>                            
+          <Typography variant="body1">Senatsverwaltung für Gesundheit, Pflege und Gleichstellung 
+</Typography>
+          <Typography variant="body1">Kontakt: <a href="https://www.berlin.de/sen/gpg/">https://www.berlin.de/sen/gpg/</a></Typography>
+          <Typography variant="body1">Umsatzsteuer-Identifikationsnummer: DE 228847810</Typography>
+          </section>
+        
       </main>
     </>
   );
