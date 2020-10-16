5077ccdb206ff42c75aed5d45a6cc93c14714f9f
Restyled.io <commits@restyled.io>
2020-10-16 16:13:42 +0000

    Restyled by prettier

--- apps/official/components/risk-levels-page/RiskLevelsPage.tsx
+++ apps/official/components/risk-levels-page/RiskLevelsPage.tsx
@@ -1,62 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Grid, Card, CardHeader, CardContent, Avatar, IconButton } from '@material-ui/core';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import { RiskTexts } from "../../models"
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Grid, Card, CardHeader, CardContent, Avatar, IconButton } from "@material-ui/core";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { RiskTexts } from "../../models";
 
 const useStyles = makeStyles({
-    action: {
-        alignSelf: "auto",
-        marginTop: 0,
-        marginLeft: "8px",
-    }
-})
+  action: {
+    alignSelf: "auto",
+    marginTop: 0,
+    marginLeft: "8px",
+  },
+});
 
 const riskLevelHeader = () => {
-    const classes = useStyles()
-    return (    
-        <CardHeader title="Risikostufen">
-            <Grid container direction="row">
-                <Grid item>
-                <IconButton                    
-                    component={Link}
-                    to="/"
-                    color="primary"
-                    aria-label="go back to map"
-                >
-                    <ArrowBackIosIcon />
-                </IconButton>
-                </Grid>
-                <Grid item>
-                    <Typography variant="h1">Risikostufen</Typography>
-                </Grid>
-            </Grid>
-        </CardHeader>
-    )
-}
+  const classes = useStyles();
+  return (
+    <CardHeader title="Risikostufen">
+      <Grid container direction="row">
+        <Grid item>
+          <IconButton component={Link} to="/" color="primary" aria-label="go back to map">
+            <ArrowBackIosIcon />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <Typography variant="h1">Risikostufen</Typography>
+        </Grid>
+      </Grid>
+    </CardHeader>
+  );
+};
 
 export const RiskLevelsPage = () => {
+  const classes = useStyles();
 
-    const classes  = useStyles()
-
-    return (
-        <div className={classes.action} id="risk-levels-page">
-            <Card >                
-                {riskLevelHeader()}
-                <Typography variant="h2">Normales Risiko</Typography>
-                <Typography>
-                    {RiskTexts.NORMAL}
-                </Typography>
-                <Typography variant="h2">Mittleres Risiko</Typography>
-                <Typography>
-                    {RiskTexts.MEDIUM}
-                </Typography>
-                <Typography variant="h2">Hohes Risiko</Typography>
-                <Typography>
-                    {RiskTexts.HIGH}
-                </Typography>
-            </Card>
-        </div>
-    )
-}
+  return (
+    <div className={classes.action} id="risk-levels-page">
+      <Card>
+        {riskLevelHeader()}
+        <Typography variant="h2">Normales Risiko</Typography>
+        <Typography>{RiskTexts.NORMAL}</Typography>
+        <Typography variant="h2">Mittleres Risiko</Typography>
+        <Typography>{RiskTexts.MEDIUM}</Typography>
+        <Typography variant="h2">Hohes Risiko</Typography>
+        <Typography>{RiskTexts.HIGH}</Typography>
+      </Card>
+    </div>
+  );
+};
