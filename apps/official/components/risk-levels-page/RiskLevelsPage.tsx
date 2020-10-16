008488a443baa3358e200c2c23fe34bfe03f38dd
Olaf Nouvortne <onouv@t-online.de>
2020-10-16 17:21:11 +0200

    -

--- apps/official/components/risk-levels-page/RiskLevelsPage.tsx
+++ apps/official/components/risk-levels-page/RiskLevelsPage.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import { Typography, Card, CardHeader, CardContent, IconButton } from '@material-ui/core';
+import { RiskTexts } from "../../models"
 
 const useStyles = makeStyles({
+    root: {
+        border: 0,
+
+    }
 })
 
 export const RiskLevelsPage = () => {
 
-    const { }  = useStyles()
+    const classes  = useStyles()
 
     return (
-        <>
-            <Typography>TO BE IMPLEMENTED</Typography>
-        </>
+        <div className={classes.root} id="risk-levels-page">
+            <Paper elevation={1}>                
+                <Typography variant="h1">Risikostufen</Typography>
+                <Typography variant="h2">Normales Risiko</Typography>
+                <Typography>
+                    {RiskTexts.NORMAL}
+                </Typography>
+                <Typography variant="h2">Mittleres Risiko</Typography>
+                <Typography>
+                    {RiskTexts.MEDIUM}
+                </Typography>
+                <Typography variant="h2">Hohes Risiko</Typography>
+                <Typography>
+                    {RiskTexts.HIGH}
+                </Typography>
+            </Paper>
+        </div>
     )
 }
