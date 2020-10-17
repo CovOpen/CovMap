eafbb2b1e5d99af8c23438fcb35272370bd3250b
Olaf Nouvortne <onouv@t-online.de>
2020-10-17 18:58:14 +0200

    showing new (shorter) texts in blue risk box in the expanded info box

--- apps/official/models.ts
+++ apps/official/models.ts
@@ -21,7 +21,7 @@ export interface RawDataEntry {
 }
 
 export enum RiskTexts {
-  NORMAL = "Die Zahl der Neuinfektionen liegt unter 20 pro 100.000 Einwohner und das Kontaktverhalten der Bevölkerung sowie die Symptomlast sind normal. Ein normales Risiko bedeutet allerdings nicht, dass gar keine Infektionen in der Region möglich sind. Bitte beachte weiterhin die AHA + L Regeln.",
-  MEDIUM = "Ein mittleres Risiko kann bei mehreren Szenarien bestehen: Entweder ist die Zahl der Neuinfektionen über 20 Neuinfektionen pro 100.000 Einwohner oder das Kontaktverhalten der Bevölkerung oder die Symptomlast ist erhöht, so dass die Zahl der Neuinfektionen demnächst weiter ansteigen könnte. Bitte die AHA + L Regeln beachten. Wir empfehlen darüber hinaus, die Anzahl der Kontakte freiwillig weitestgehend zu reduzieren.",
-  HIGH = "Die Zahl der Neuinfektionen ist stark erhöht und liegt über 50 pro 100.000 Einwohnern. Bitte weiterhin die AHA + L Regeln beachten und die Anzahl der Kontakte auf das Allernötigste reduzieren.",
+  NORMAL = "Die Zahl der Neuinfektionen ist niedrig, das Kontaktverhalten ist ausreichend reduziert und die Symptomlast ist normal. Ein normales Risiko bedeutet allerdings nicht, dass gar keine Infektionen in der Region möglich sind",
+  MEDIUM = "Die Zahl der Neuinfektionen oder das Kontaktverhalten bzw. die Symptomlast der Bevölkerung ist erhöht, so dass die Zahl der Neuinfektionen demnächst weiter ansteigen könnte. Wir empfehlen, die Anzahl der Kontakte freiwillig weitestgehend zu reduzieren.",
+  HIGH = "Die Zahl der Neuinfektionen ist stark erhöht. Wir empfehlen, die Anzahl der Kontakte freiwillig auf das Allernötigste zu reduzieren."
 }
