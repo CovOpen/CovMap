6e816f4ecd1b2cb70f039bdb60d6eb9eae8ec25a
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-10-17 15:01:40 +0200

    add version number to menu

--- src/version.ts
+++ src/version.ts
@@ -3,6 +3,6 @@
  * Changes wont take effect in any release.
  * This are just dummy values for use in development.
  */
-export const VERSION = "1.0.0";
+export const VERSION = "DEV";
 export const HASH_SHORT = "4172d22";
 export const HASH_LONG = "4172d2239ad282e21e82e93f5cd676f37af0b72c";
