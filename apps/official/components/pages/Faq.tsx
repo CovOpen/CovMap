0c65151627975e4f6beda6427744b3ebf445669d
Restyled.io <commits@restyled.io>
2020-10-18 18:55:24 +0000

    Restyled by prettier

--- apps/official/components/pages/Faq.tsx
+++ apps/official/components/pages/Faq.tsx
@@ -13,7 +13,7 @@ const useStyles = makeStyles(() => ({
 const FaqAccordion: React.FC<{ title: string; content: string | ReactElement }> = (props) => {
   return (
     <Accordion>
-      <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
         <Typography variant="h3" style={{ margin: "10px" }}>
           {props.title}
         </Typography>
@@ -63,7 +63,6 @@ export const Faq: React.FC = () => {
                   </div>
                   <div className={classes.textBlock}>
                     Mit der CovMap möchten wir an eine freiwillige Reduzierung von Kontakten appellieren und
-
                     gleichermaßenaufzeigen, wo diese nach unserer Meinung besonders notwendig sind.
                   </div>
                 </div>
@@ -104,43 +103,49 @@ export const Faq: React.FC = () => {
                 <div style={{ display: "flex", flexDirection: "column" }}>
                   <div className={classes.textBlock}>
                     Damit das Virus übertragen werden kann, müssen sich Menschen begegnen. Wenige Tage nach der
-                Infektionberichten viele infizierte Personen über Symptome. Damit stellen der Kontakt zwischen Menschen und das
-                Bemerken von Symptomen die zwei frühesten beobachtbaren Ereignisse einer Infektion dar. Und genau da
-                setzt unser Projekt an.
-                </div>
-                  <div className={classes.textBlock}>
-                Wir charakterisieren aus anonymisierten Smartphone-Daten (ermittelt aus GPS Daten) das Kontaktverhalten.
-                Des weiteren werten wir selbstberichtete Symptome aus. Nun ist es so, dass nicht jeder Kontakt zu einer
-                Virusübertragung führt und auch Symptome nicht beweisend für eine Infektion mit dem Coronavirus sind.
-                Wertet man jedoch die Daten von vielen Personen aus, dann erreicht man dadurch erstens eine
-                Anonymisierung und man kann mit diesen Daten eine Prognose der zukünftigen Fallzahlen erstellen.
-                </div>
-                  <div className={classes.textBlock}>
-                Der von uns entwickelte Kontakt-Index ist aktueller als die 7-Tages-Inzidenz und quasi ein
-                Frühwarnsystem. Er gibt die Anzahl der durchschnittlichen Kontakte pro Person und Region in den letzten
-                24h Stunden an. In ersten Analysen konnten wir zeigen, dass eine hohe Anzahl an Kontakten mit einem
-                vermehrten Infektionsgeschehen korreliert.
-                <Typography variant="caption" style={{ verticalAlign: "super" }}>
-                  <a href="https://www.medrxiv.org/content/10.1101/2020.10.02.20188136v2"target="_blank"
-                        rel="noreferrer"
-                      >[1]</a>
-                </Typography>
+                    Infektionberichten viele infizierte Personen über Symptome. Damit stellen der Kontakt zwischen
+                    Menschen und das Bemerken von Symptomen die zwei frühesten beobachtbaren Ereignisse einer Infektion
+                    dar. Und genau da setzt unser Projekt an.
                   </div>
-                <div className={classes.textBlock}>
-                Von anderen Autoren wurde gezeigt, dass die Auswertung von Symptomen zur Vorhersage von lokalen
-                Ausbrüchen verwendet werden kann. Über die Charité CovApp werden von vielen Benutzern Symptomantworten
-                eingegeben und wir planen, dies in ein verbessertes Modell einfließen zu lassen. Zeitlich ist die
-                Symptomlast als Prädiktor für das Infektionsgeschehen zwischen Kontakt-Index und 7-Tages-Inzidenz
-                einzuordnen.
-                </div>
                   <div className={classes.textBlock}>
-                Der große Vorteil der Auswertung von Kontakten und Symptomen liegt darin, dass die Daten ohne
-                Verzögerung analysiert werden können und daher ohne den Meldeverzug der offiziellen RKI Daten zur
-                Verfügung stehen. Dadurch können wir die Bevölkerung schneller auf die Möglichkeit eines erhöhten
-                regionalen Risikos hinweisen.
-                </div>
+                    Wir charakterisieren aus anonymisierten Smartphone-Daten (ermittelt aus GPS Daten) das
+                    Kontaktverhalten. Des weiteren werten wir selbstberichtete Symptome aus. Nun ist es so, dass nicht
+                    jeder Kontakt zu einer Virusübertragung führt und auch Symptome nicht beweisend für eine Infektion
+                    mit dem Coronavirus sind. Wertet man jedoch die Daten von vielen Personen aus, dann erreicht man
+                    dadurch erstens eine Anonymisierung und man kann mit diesen Daten eine Prognose der zukünftigen
+                    Fallzahlen erstellen.
+                  </div>
                   <div className={classes.textBlock}>
-                Das CovMap Projekt wird zur Zeit aktiv weiterentwickelt und verbessert.</div>
+                    Der von uns entwickelte Kontakt-Index ist aktueller als die 7-Tages-Inzidenz und quasi ein
+                    Frühwarnsystem. Er gibt die Anzahl der durchschnittlichen Kontakte pro Person und Region in den
+                    letzten 24h Stunden an. In ersten Analysen konnten wir zeigen, dass eine hohe Anzahl an Kontakten
+                    mit einem vermehrten Infektionsgeschehen korreliert.
+                    <Typography variant="caption" style={{ verticalAlign: "super" }}>
+                      <a
+                        href="https://www.medrxiv.org/content/10.1101/2020.10.02.20188136v2"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        [1]
+                      </a>
+                    </Typography>
+                  </div>
+                  <div className={classes.textBlock}>
+                    Von anderen Autoren wurde gezeigt, dass die Auswertung von Symptomen zur Vorhersage von lokalen
+                    Ausbrüchen verwendet werden kann. Über die Charité CovApp werden von vielen Benutzern
+                    Symptomantworten eingegeben und wir planen, dies in ein verbessertes Modell einfließen zu lassen.
+                    Zeitlich ist die Symptomlast als Prädiktor für das Infektionsgeschehen zwischen Kontakt-Index und
+                    7-Tages-Inzidenz einzuordnen.
+                  </div>
+                  <div className={classes.textBlock}>
+                    Der große Vorteil der Auswertung von Kontakten und Symptomen liegt darin, dass die Daten ohne
+                    Verzögerung analysiert werden können und daher ohne den Meldeverzug der offiziellen RKI Daten zur
+                    Verfügung stehen. Dadurch können wir die Bevölkerung schneller auf die Möglichkeit eines erhöhten
+                    regionalen Risikos hinweisen.
+                  </div>
+                  <div className={classes.textBlock}>
+                    Das CovMap Projekt wird zur Zeit aktiv weiterentwickelt und verbessert.
+                  </div>
                 </div>
               </Typography>
             }
