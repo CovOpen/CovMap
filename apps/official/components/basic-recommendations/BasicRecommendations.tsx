bf62890f23e49d62b9537e7d5f574da3c2833411
Olaf Nouvortne <onouv@t-online.de>
2020-10-19 14:06:23 +0200

    layout info box, layout what-can-i-do-page
    -
    
    -
    
    -
    
    -
    
    improving info box layout. removing district id from info box header. fixing what-can-i-do page formatting

--- apps/official/components/basic-recommendations/BasicRecommendations.tsx
+++ apps/official/components/basic-recommendations/BasicRecommendations.tsx
@@ -28,7 +28,7 @@ const useStyles = makeStyles({
     marginLeft: "8px",
   },
   leftText: {
-    textAlign: "left",
+    textAlign: "left"
   },
   subHeader: {
     textAlign: "left",
@@ -66,8 +66,8 @@ const CountyTeaser = ({ county, url }: { county: string; url: string }): JSX.Ele
           </Grid>
           <Grid item xs={2}>
             <IconButton href={url} target="_blank" color="primary" aria-label="show risk level explanations">
-              <ArrowForwardIosIcon fontSize="small" />
-            </IconButton>
+              <ArrowForwardIosIcon fontSize="small"/>
+            </IconButton>            
           </Grid>
         </Grid>
       </CardContent>
