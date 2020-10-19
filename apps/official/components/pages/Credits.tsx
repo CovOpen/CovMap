9bc8327bbe8cb4e348ae3827600e983c771760eb
Restyled.io <commits@restyled.io>
2020-10-19 20:57:11 +0000

    Restyled by prettier

--- apps/official/components/pages/Credits.tsx
+++ apps/official/components/pages/Credits.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-const contributers = require('json-loader!./../../../../.all-contributorsrc');
+const contributers = require("json-loader!./../../../../.all-contributorsrc");
 
 export const Credits = () => {
   return (
@@ -10,29 +10,31 @@ export const Credits = () => {
           <Typography variant="h1">Danksagung</Typography>
         </section>
         <section>
-        <Typography variant="body1">
-            An der Entwicklung dieser App sind und waren verschiedene Personen und Unternehmen beteiligt, diesen möchten wir an dieser Stelle danken.
+          <Typography variant="body1">
+            An der Entwicklung dieser App sind und waren verschiedene Personen und Unternehmen beteiligt, diesen möchten
+            wir an dieser Stelle danken.
           </Typography>
           <Typography variant="h2">Personen</Typography>
           <Typography variant="body1">
-
             <ul>
-            {
-              contributers.contributors.map(el => <li><a href="{el.profile}" target='_blank' rel="noopener">{el.name}</a></li>)
-            }
+              {contributers.contributors.map((el) => (
+                <li>
+                  <a href="{el.profile}" target="_blank" rel="noopener">
+                    {el.name}
+                  </a>
+                </li>
+              ))}
             </ul>
           </Typography>
         </section>
         <section>
           <Typography variant="h2">Unternehmen</Typography>
           <Typography variant="body1">
-            Port Blue Sky für die technische Umsetzung der App durch Ihre Mitarbeiter sowie das Organisieren von weiterer Unterstützung durch Voluntäre aus der Node.js Community.
-
+            Port Blue Sky für die technische Umsetzung der App durch Ihre Mitarbeiter sowie das Organisieren von
+            weiterer Unterstützung durch Voluntäre aus der Node.js Community.
           </Typography>
           <Typography variant="body1">
-            KeyCDN für die Bereitstellung ihrer Dienste.
-            OpenStreetMap für die Bereitstellung ihrer Dienste.
-
+            KeyCDN für die Bereitstellung ihrer Dienste. OpenStreetMap für die Bereitstellung ihrer Dienste.
           </Typography>
         </section>
       </main>
