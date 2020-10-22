d3a2e3fd949016adca62b4337f2766b5ed10c5a1
DanielHabenicht <daniel-habenicht@outlook.de>
2020-10-22 20:39:22 +0200

    feat: first release (#93)
    * Initial setup of testcafe and very small first test
    
    * Restyled by prettier
    
    * consolidate pr checks
     -  run tests with pr check
     - use one build prod command
     - consolidate pr check to only build once
    
    * Update pr-check.yml
    
    * Update pr-check.yml
    
    * fix tests
    
    * Update pr-check.yml
    
    * Update pr-check.yml
    
    * add cache for base step
    
    * fix testcafe base url
    
    * fix caching and typos
    
    * feat: add semantic release
    
    * add version number to menu
    
    * delete aws.yml
    
    * fix: do not execute steps if not released
    
    * Update release.yml
    
    * Update release.yml
    
    * Update release.yml
    
    * Update release.yml
    
    * change repository for testing
    
    * fix: ready for production
     - clean release.yml
     - remove package-lock.json
    
    * fetch tags
    
    * fix fetch tags
    
    * fix changelog url
    
    * remove fetch tags
    
    * fix: changelog path
    
    * fix changelog script
    
    * remove log action
    
    this should not trigger a release
    
    * add version number
    
    * fix: add dist files to github release
    
    * fix: semantic release flow
    
    * format
    
    * fix: release dist zip
    
    * Update package.json
    
    Co-authored-by: Christoph Wersal <christoph.wersal@tngtech.com>
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: ChristophWersal <45358216+ChristophWersal@users.noreply.github.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -19,6 +19,7 @@ import { useTranslation } from "react-i18next";
 import { config } from "app-config/index";
 import { Drawer, useMediaQuery } from "@material-ui/core";
 import { CloseRounded } from "@material-ui/icons";
+import { VERSION, HASH_LONG, HASH_SHORT } from 'src/version';
 
 const Logo = config.ui?.Logo;
 
@@ -58,7 +59,12 @@ const useStyles = makeStyles((theme) => ({
     width: "auto",
     marginTop: "9px",
   },
-
+  version: {
+    fontSize: "12px",
+    margin: "auto",
+    marginBottom: "10px",
+    marginTop: 0
+  },
   drawer: {
     touchAction: "none",
   },
@@ -188,12 +194,14 @@ export const NavBar = ({ showSearch }: NavBarProps) => {
             <MenuCloseButton handleClose={handleClose} />
           </Toolbar>
           <NavMenuContent />
-
-          {(Logo && (
+          {
+            (Logo && (
             <div className={classes.drawerIcon}>
               <Logo />
             </div>
-          )) || <img src={config.buildJSON.logoSrc} className={classes.logo} />}
+          )) || <img src={config.buildJSON.logoSrc} className={classes.logo} />
+        }
+          <div className={classes.version}>{"v" + VERSION} - <a href={"https://github.com/CovOpen/CovMapper/commit/" + HASH_LONG} target='_blank' rel="noopener">{HASH_SHORT}</a></div>
         </Drawer>
       </Toolbar>
     </AppBar>
