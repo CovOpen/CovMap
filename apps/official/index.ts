d073152a94e038e652f0cf76a86f1ea6cd06278a
Sebastian Herrlinger <hasta84@gmail.com>
2020-10-22 17:37:39 +0200

    Reorder pages for menu entries and add translations

--- apps/official/index.ts
+++ apps/official/index.ts
@@ -49,10 +49,22 @@ export const config: AppConfig = {
   content: {
     pages: [
       {
+        id: "charts",
+        title: (t) => t("translation:pages.charts"),
+        route: "/charts",
+        Component: Charts,
+      },
+      {
         id: "faq-page",
         title: (t) => t("translation:pages.faq"),
         route: "/faq",
         Component: Faq,
+      },
+      {
+        id: "credits",
+        title: (t) => t("translation:pages.about"),
+        route: "/credits",
+        Component: Credits,
       },
       {
         id: "imprint-page",
@@ -73,12 +85,6 @@ export const config: AppConfig = {
         Component: Privacy,
       },
       {
-        id: "credits",
-        title: "Über die CovMap",
-        route: "/credits",
-        Component: Credits,
-      },
-      {
         id: "basic-recommendations",
         title: "Empfehlungen",
         route: "/recommendations",
@@ -91,12 +97,6 @@ export const config: AppConfig = {
         route: "/risk-levels",
         Component: RiskLevelsPage,
         hidden: true,
-      },
-      {
-        id: "charts",
-        title: "Deutschlandweite Graphen",
-        route: "/charts",
-        Component: Charts,
       },
       {
         id: "rki-page",
