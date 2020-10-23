44c432a6cc2803fb61948b74f6ae01acc10bec4a
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-10-23 18:05:38 +0200

    Update ContactBehavior.tsx

--- apps/official/components/pages/ContactBehavior.tsx
+++ apps/official/components/pages/ContactBehavior.tsx
@@ -16,25 +16,23 @@ const ContactBehavior = () => {
           <a target="_blank" href="https://www.medrxiv.org/content/10.1101/2020.10.02.20188136v2" rel="noreferrer">
             Kontakt-Index
           </a>{" "}
-          entwickeln, der eine hohe Korrelation mit der effektiven Reproduktionszahl (R) des Coronavirus aufweist, und
-          das 6 Tage früher!
-        </Typography>
-        <Typography>
-          Der R-Wert ist eine wichtige Maßzahl zur Steuerung der Corona Maßnahmen. Er besagt, wie viele Personen von
-          einer infizierten Person im Durchschnitt angesteckt werden. Wenn der R Wert 1 beträgt, bedeutet dies, dass
-          eine infizierte Person eine weitere ansteckt. Ziel ist es den R Wert unter 1 zu bekommen, einer infizierte
-          Person also im Durchschnitt weniger als eine Person ansteckt.
-        </Typography>
-        <Typography>
-          Die Berechnung des{" "}
+          entwickeln, der eine hohe Korrelation mit der {" "}
           <a
             target="_blank"
             href="https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Projekte_RKI/R-Wert-Erlaeuterung.pdf?__blob=publicationFile"
             rel="noreferrer"
           >
-            R-Wertes
-          </a>{" "}
-          ist jedoch nicht trivial. Zur Berechnung greift das RKI auf das sogenannte „Nowcast“ Model zurück.
+            effektiven Reproduktionszahl (R)
+          </a>{" "} des Coronavirus aufweist, und das 6 Tage früher!
+        </Typography>
+        <Typography>
+          Der R-Wert ist eine wichtige Maßzahl zur Steuerung der Corona Maßnahmen. Er besagt, wie viele Personen von
+          einer infizierten Person im Durchschnitt angesteckt werden. Wenn der R-Wert 1 beträgt, bedeutet dies, dass
+          eine infizierte Person eine weitere ansteckt. Ziel ist es, durch Maßnahmen einen R Wert unter 1 zu erreichen, d.h., dass eine infizierte
+          Person im Durchschnitt weniger als eine weitere Person ansteckt. Ein R-Wert oberhalb von 1 führt dagegen zu einem exponentiellem Anstieg der Fallzahlen.
+        </Typography>
+        <Typography>
+          Die Berechnung des R-Werts ist jedoch nicht trivial. Zur Berechnung greift das RKI auf das sogenannte „Nowcast“ Model zurück.
           Das Problem des R-Werts ist es, dass
           das Infektionsgeschehen mit mehreren Tage Verzögerung abgebildet wird. Dies erscheint auch logisch, wenn man
           bedenkt, dass man nach einer Ansteckung mit dem Coronavirus nicht sofort Symptome entwickelt. Falls sich eine Person einem Labortest unterzieht, dauert es in der Regel mehrere Tage, bis das Ergebnis dem Robert-Koch-Institut mitgeteilt wird.
