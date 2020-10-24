b353f09b927897ac65643b6ee706ea5a7c9eb782
Sebastian Herrlinger <hasta84@gmail.com>
2020-10-24 17:24:10 +0200

    chore: remove double exclusion check (#227)
    * remove double exclusion check
    
    * Restyled by prettier (#228)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/components/WelcomeInfoButton.tsx
+++ src/components/WelcomeInfoButton.tsx
@@ -6,16 +6,10 @@ import { useSelector } from "react-redux";
 import { useThunkDispatch } from "../useThunkDispatch";
 import { AppApi } from "../state/app";
 import { State } from "../state";
-import { config } from "app-config/index";
 
 export function WelcomeInfoButton() {
   const dispatch = useThunkDispatch();
   const currentVisual = useSelector((state: State) => state.app.currentVisual);
-  const InfoComponent = config.visuals[currentVisual].InfoComponent;
-
-  if (InfoComponent === undefined) {
-    return null;
-  }
 
   const onInfoClick = () => {
     dispatch(AppApi.setInfoDialog(currentVisual, false));
