8e9db0d79808b27fbe8ea8bca50d04c6f117f0df
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-10-26 23:14:18 +0100

    wip

--- testcafe/introScreens.spec.ts
+++ testcafe/introScreens.spec.ts
@@ -1,5 +1,6 @@
 import { Selector } from "testcafe";
 import { Config } from "./testcafe.config";
+import percySnapshot from "@percy/testcafe";
 
 fixture`Intro screens test`.page`${Config.baseUrl}`;
 
@@ -13,10 +14,10 @@ test("Finish intro screens", async (t) => {
   const titleTextForPostalCode = "Für Dein regionales Risiko brauchen wir noch die Postleitzahl Deines Wohnortes";
 
   let firstPageView = await t.wait(1);
-  await Config.percySnapshot(firstPageView, "First Page View");
+  // await percySnapshot("First Page View");
 
   let firstDialog = await t.expect(websiteText).contains(titleForIntro1).click(nextButton);
-  await Config.percySnapshot(firstDialog, "First Dialog");
+  // await percySnapshot("First Dialog");
 
   await t.expect(websiteText).contains(titleForIntro2).click(nextButton);
 
@@ -25,5 +26,5 @@ test("Finish intro screens", async (t) => {
   await t.expect(websiteText).contains(titleTextForPostalCode).pressKey("1 2 3 4 5 tab space tab tab enter");
 
   let finalSite = await t.expect(websiteText).notContains(titleTextForPostalCode);
-  await Config.percySnapshot(finalSite, "Final Site");
+  await percySnapshot("Final Site");
 });
--- testcafe/testcafe.config.js
+++ testcafe/testcafe.config.js
@@ -2,7 +2,7 @@ import percySnapshot from "@percy/testcafe";
 
 export class Config {
   static baseUrl = "http://localhost:8080";
-  static percySnapshot = (name: string, options: {} = {}) => {
+  static percySnapshot = (name, options) => {
     percySnapshot(name, {
       widths: [768, 992, 1200],
       ...options,
