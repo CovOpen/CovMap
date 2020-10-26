bec8dfd554cd02d549d6b11960aeaa6b02ad3496
Restyled.io <commits@restyled.io>
2020-10-26 13:51:43 +0000

    Restyled by prettier

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,16 @@ export const App = () => {
         path={page.route}
         key={page.id}
         component={() => (
-          <div style={{ zIndex: 1095, backgroundColor: "white", width: "100%", height: "100%", flexShrink: 0, overflow: "hidden" }}>
+          <div
+            style={{
+              zIndex: 1095,
+              backgroundColor: "white",
+              width: "100%",
+              height: "100%",
+              flexShrink: 0,
+              overflow: "hidden",
+            }}
+          >
             <div style={{ width: "100%", height: "100%", overflow: "auto" }}>
               <page.Component />
             </div>
