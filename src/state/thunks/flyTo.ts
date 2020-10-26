fab4363086086e64c40c984ea2f04555fdfb27a4
Sebastian Herrlinger <hasta84@gmail.com>
2020-10-26 18:52:13 +0100

    fix interpolator import

--- src/state/thunks/flyTo.ts
+++ src/state/thunks/flyTo.ts
@@ -1,20 +1,11 @@
 import { ReduxDispatch } from "../../useThunkDispatch";
 import { AppApi } from "../app";
 
-let FlyToInterpolator = null;
-async function loadFlyTo() {
-  if (FlyToInterpolator !== null) {
-    return;
-  }
-  const { default: FlyTo } = await import(
-    /* webpackChunkName: "mapgl" */ "react-map-gl/dist/es6/utils/transition/viewport-fly-to-interpolator"
-  );
-  FlyToInterpolator = FlyTo;
-}
-
 export function flyTo(lng: number, lat: number) {
-  loadFlyTo();
   return async (dispatch: ReduxDispatch, getState) => {
+    const { default: FlyToInterpolator } = await import(
+      /* webpackChunkName: "mapgl" */ "react-map-gl/dist/es6/utils/transition/viewport-fly-to-interpolator"
+    );
     const state = getState().app;
 
     const newViewport = {
