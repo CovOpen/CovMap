28191f0431e70c31ca926cd360a6e7b77f3b518a
Christoph Wersal <christoph.wersal@tngtech.com>
2020-10-27 19:24:51 +0100

    fix: remove attribute that caused a console error in the browser

--- apps/official/components/CovMapFeatureInfo.tsx
+++ apps/official/components/CovMapFeatureInfo.tsx
@@ -223,7 +223,7 @@ export const CovMapFeatureInfo = ({ rawData }: FeatureInfoProps) => {
         <Grid item xs={12}>
           {RiskRecommendation({ riskScore })}
         </Grid>
-        <Grid item xs={12} alignContent="stretch">
+        <Grid item xs={12}>
           <ContactBehaviorCategory />
         </Grid>
         <Grid item xs={12}>
