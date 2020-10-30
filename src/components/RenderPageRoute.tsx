6259896f8d9d66731f0212cd3d11e411cf97af5e
Sebastian Herrlinger <hasta84@gmail.com>
2020-10-30 13:56:35 +0100

    incorporate: use full screen on mobile on menu pages and force title arrow to be next to title

--- src/components/RenderPageRoute.tsx
+++ src/components/RenderPageRoute.tsx
@@ -1,28 +1,41 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
 import type { AppPage } from "../app-config.types";
 
+const useStyles = makeStyles((theme) => ({
+  outerPageDiv: {
+    zIndex: 1095,
+    backgroundColor: "white",
+    width: "100%",
+    height: "100%",
+    boxSizing: "border-box",
+    flexShrink: 0,
+    overflow: "hidden",
+    paddingTop: "64px",
+    [theme.breakpoints.down("xs")]: {
+      // on mobile devices
+      paddingTop: "0 !important",
+    },
+  },
+  innerPageDiv: {
+    width: "100%",
+    height: "100%",
+    overflow: "auto",
+  },
+}));
 
 export const RenderPageRoute = ({ page }: { page: AppPage }) => {
+  const classes = useStyles();
+  
   return (
     <Route
       path={page.route}
       key={page.id}
       component={() => (
-        <div
-          style={{
-            zIndex: 1095,
-            backgroundColor: "white",
-            width: "100%",
-            height: "100%",
-            boxSizing: "border-box",
-            flexShrink: 0,
-            overflow: "hidden",
-            paddingTop: "64px",
-          }}
-        >
-          <div style={{ width: "100%", height: "100%", overflow: "auto" }}>
+        <div className={classes.outerPageDiv}>
+          <div className={classes.innerPageDiv}>
             <page.Component />
           </div>
         </div>
