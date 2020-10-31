8d8509093e6eae9fcfd9ea0a6b3a9aec8c8bcddb
ChristophWersal <45358216+ChristophWersal@users.noreply.github.com>
2020-10-31 11:27:04 +0100

    fix: replace the search for texts in the html text with testcafe selectors (#306)

--- testcafe/introScreens.spec.ts
+++ testcafe/introScreens.spec.ts
@@ -1,24 +1,27 @@
-import { Selector } from "testcafe";
 import { Config } from "./testcafe.config";
+import { buttonWithText, textExists } from "./testUtils";
 
 fixture`Intro screens test`.page`${Config.baseUrl}`;
 
 test("Finish intro screens", async (t) => {
-  const nextButton = Selector("a").withAttribute("role", "button").child().withText("Weiter");
-  const websiteText = Selector("html").textContent;
+  const nextButton = buttonWithText("Weiter");
 
   const titleForIntro1 = "Willkommen bei der CovMap";
   const titleForIntro2 = "Was ist die CovMap?";
   const titleForIntro3 = "Was zeigt mir die CovMap an?";
   const titleTextForPostalCode = "Für Dein regionales Risiko brauchen wir noch die Postleitzahl Deines Wohnortes";
 
-  await t.expect(websiteText).contains(titleForIntro1).click(nextButton);
+  await t.expect(textExists(titleForIntro1)).ok();
+  await t.click(nextButton);
 
-  await t.expect(websiteText).contains(titleForIntro2).click(nextButton);
+  await t.expect(textExists(titleForIntro2)).ok();
+  await t.click(nextButton);
 
-  await t.expect(websiteText).contains(titleForIntro3).click(nextButton);
+  await t.expect(textExists(titleForIntro3)).ok();
+  await t.click(nextButton);
 
-  await t.expect(websiteText).contains(titleTextForPostalCode).pressKey("1 2 3 4 5 tab space tab tab enter");
+  await t.expect(textExists(titleTextForPostalCode)).ok();
+  await t.pressKey("1 2 3 4 5 tab space tab tab enter");
 
-  await t.expect(websiteText).notContains(titleTextForPostalCode);
+  await t.expect(textExists(titleTextForPostalCode)).notOk();
 });
