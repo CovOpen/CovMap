867a040a8562f7326375f93c5092309d13ebb9c8
Christoph Wersal <christoph.wersal@tngtech.com>
2020-11-01 17:57:15 +0100

    feat: add second graph with actual and predicted case numbers

--- apps/official/components/pages/Charts/Chart.tsx
+++ apps/official/components/pages/Charts/Chart.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { ChartConfig } from "app-config/components/pages/Charts/ChartConfig";
-
 import CartesianGrid from "recharts/es6/cartesian/CartesianGrid";
 import Legend from "recharts/es6/component/Legend";
 import Line from "recharts/es6/cartesian/Line";
@@ -11,46 +9,51 @@ import XAxis from "recharts/es6/cartesian/XAxis";
 import YAxis from "recharts/es6/cartesian/YAxis";
 
 export type ChartData = Array<{ datum: string } & { [key: string]: number | null }>;
+export type LineConfig = { dataKey: string; name: string; yAxis?: [number, number]; ticks?: number[] };
+export type ChartConfig = { line1: LineConfig; line2: LineConfig };
 
-export const Chart: React.FC<{ data: ChartData; chartConfig: ChartConfig }> = ({ data, chartConfig }) => (
-  <ResponsiveContainer>
-    <LineChart
-      data={data}
-      margin={{
-        top: 20,
-        right: 0,
-        left: 0,
-        bottom: 0,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey={"datum"} interval={"preserveEnd"} angle={-8} />
-      <Tooltip />
-      <Legend />
-      {chartConfig.ci && <YAxis yAxisId="left" domain={[0, 650]} />}
-      {chartConfig.ci && (
-        <Line
-          yAxisId="left"
-          type="monotone"
-          dataKey={"CI"}
-          stroke="#8884d8"
-          strokeWidth="4px"
-          name="Kontaktindex C"
-          dot={null}
-        />
-      )}
-      {chartConfig.r && <YAxis yAxisId="right" domain={[0, 3.21]} orientation="right" />}
-      {chartConfig.r && (
-        <Line
-          yAxisId="right"
-          type="monotone"
-          dataKey={"R"}
-          stroke="#f01a8d"
-          strokeWidth="4px"
-          name="Reproduktionszahl R"
-          dot={null}
-        />
-      )}
-    </LineChart>
-  </ResponsiveContainer>
+export const Chart: React.FC<{ data: ChartData; chartConfig: ChartConfig }> = ({
+  data,
+  chartConfig: { line1, line2 },
+}) => (
+  <div style={{ position: "relative", width: "100%", height: "auto", padding: "62% 0 0 0" }}>
+    <div style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0 }}>
+      <ResponsiveContainer>
+        <LineChart
+          data={data}
+          margin={{
+            top: 20,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey={"datum"} interval={"preserveEnd"} angle={-8} />
+          <Tooltip />
+          <Legend />
+          <YAxis yAxisId="left" domain={line1.yAxis} ticks={line1.ticks} />
+          <Line
+            yAxisId="left"
+            type="monotone"
+            dataKey={line1.dataKey}
+            stroke="#8884d8"
+            strokeWidth="4px"
+            name={line1.name}
+            dot={null}
+          />
+          <YAxis yAxisId="right" domain={line2.yAxis} orientation="right" ticks={line2.ticks} />
+          <Line
+            yAxisId="right"
+            type="monotone"
+            dataKey={line2.dataKey}
+            stroke="#f01a8d"
+            strokeWidth="4px"
+            name={line2.name}
+            dot={null}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  </div>
 );
--- apps/official/components/pages/Charts/ChartsPage.tsx
+++ apps/official/components/pages/Charts/ChartsPage.tsx
@@ -6,9 +6,8 @@ import { AppApi } from "../../../../../src/state/app";
 
 import { formatUTCDate } from "../../../../../src/lib/formatUTCDate";
 import { NavigationTitle } from "../../NavigationTitle";
-import { Card, FormControl, InputLabel, MenuItem, Select, Typography } from "@material-ui/core";
-import { Chart, ChartData } from "app-config/components/pages/Charts/Chart";
-import { chartConfigs } from "app-config/components/pages/Charts/ChartConfig";
+import { Card, Typography } from "@material-ui/core";
+import { Chart, ChartData, LineConfig } from "app-config/components/pages/Charts/Chart";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
@@ -17,6 +16,9 @@ const useStyles = makeStyles(() => ({
     background: "#2979ff",
     color: "white",
     textTransform: "none",
+  },
+  section: {
+    maxWidth: "800px !important",
   },
 }));
 
@@ -28,9 +30,10 @@ export const ChartsPage: React.FC = () => {
   const dataUrl =
     process.env.NODE_ENV === "production"
       ? `https://data.covmap.de/data/graph-${dateKey}.json`
-      : "/data/charts-data.json";
+      : "/data/chart1-data.json";
+  const data2Url = "/data/chart2-data.json";
   const [data, setData] = useState<ChartData>();
-  const [chartConfigKey, setChartConfigKey] = useState<string>(Object.keys(chartConfigs)[0]);
+  const [data2, setData2] = useState<ChartData>();
 
   useEffect(() => {
     dispatch(AppApi.pushLoading("charts-data"));
@@ -42,43 +45,65 @@ export const ChartsPage: React.FC = () => {
       });
   }, []);
 
-  if (!data) {
+  useEffect(() => {
+    dispatch(AppApi.pushLoading("chart2-data"));
+    fetch(data2Url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData2(data);
+        dispatch(AppApi.popLoading("chart2-data"));
+      });
+  }, []);
+
+  if (!data || !data2) {
     return null;
   }
 
+  const ciConfig: LineConfig = { dataKey: "CI", name: "Kontaktindex C", yAxis: [0, 650] };
+  const rConfig: LineConfig = { dataKey: "R", name: "Reproduktionszahl R", yAxis: [0, 3.21], ticks: [0, 1, 2, 3] };
+
+  const values: number[] = data2.map(({ Cases, PredictedCases }) => Math.max(Cases ?? 0, PredictedCases ?? 0));
+  const yAxisMaxCases = Math.max(...values);
+  const casesConfig: LineConfig = { dataKey: "Cases", name: "Fälle", yAxis: [0, yAxisMaxCases] };
+  const predictionCasesConfig: LineConfig = {
+    dataKey: "PredictedCases",
+    name: "Vorhersage",
+    yAxis: [0, yAxisMaxCases],
+  };
+
   return (
     <main className="sections">
-      <section>
+      <section className={classes.section}>
         <NavigationTitle title={"Deutschlandweite Graphen"} />
       </section>
-      <section>
-        <Card className={classes.teaser}>
+
+      <section className={classes.section}>
+        <Card className={classes.teaser} elevation={0}>
           <Typography style={{ margin: "8px" }}>Hier steht ein netter Text um die Graphen zu erklären</Typography>
         </Card>
       </section>
-      <section>
-        <FormControl style={{ minWidth: "300px" }}>
-          <InputLabel id="chart-select-label">Welche Graphen soll angezeigt werden?</InputLabel>
-          <Select
-            labelId="chart-select-label"
-            value={chartConfigKey}
-            onChange={(event) => setChartConfigKey(event.target.value as string)}
-          >
-            {Object.keys(chartConfigs).map((key) => (
-              <MenuItem key={key} value={key}>
-                {key}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+
+      <section className={classes.section}>
+        <Typography style={{ margin: "8px" }}>Oder auch ein Text ohne blaue Box</Typography>
       </section>
-      <section style={{ overflowX: "auto" }}>
+
+      <section className={classes.section} style={{ overflowX: "auto" }}>
         <div style={{ minWidth: "450px" }}>
-          <div style={{ position: "relative", width: "100%", height: "auto", padding: "62% 0 0 0" }}>
-            <div style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0 }}>
-              <Chart data={data} chartConfig={chartConfigs[chartConfigKey]} />
-            </div>
-          </div>
+          <Chart data={data} chartConfig={{ line1: ciConfig, line2: rConfig }} />
+        </div>
+      </section>
+
+      <section className={classes.section}>
+        <Typography variant="h1">Zwischenüberschrift</Typography>
+        <Typography style={{ margin: "8px" }}>
+          Im folgenden Diagram sind die bisherigen und die von unserem Model für die nächste Woche vorhergesagten
+          Fallzahlen abgebildet.
+        </Typography>
+      </section>
+
+      <section className={classes.section} style={{ overflowX: "auto" }}>
+        <div style={{ minWidth: "450px" }}>
+          <Chart data={data2} chartConfig={{ line1: casesConfig, line2: predictionCasesConfig }} />
         </div>
       </section>
     </main>
