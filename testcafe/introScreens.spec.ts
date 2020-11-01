5d0adcb40278d652a7285b637eafd2d9b256d803
cfdd73 <eric@louvard.de>
2020-11-01 22:20:26 +0100

    add screenshots

--- testcafe/introScreens.spec.ts
+++ testcafe/introScreens.spec.ts
@@ -11,17 +11,23 @@ test("Finish intro screens", async (t) => {
   const titleForIntro3 = "Was zeigt mir die CovMap an?";
   const titleTextForPostalCode = "Für Dein regionales Risiko brauchen wir noch die Postleitzahl Deines Wohnortes";
 
+  await t.takeScreenshot();
+
   await t.expect(textExists(titleForIntro1)).ok();
   await t.click(nextButton);
 
+  await t.takeScreenshot();
   await t.expect(textExists(titleForIntro2)).ok();
   await t.click(nextButton);
 
+  await t.takeScreenshot();
   await t.expect(textExists(titleForIntro3)).ok();
   await t.click(nextButton);
 
+  await t.takeScreenshot();
   await t.expect(textExists(titleTextForPostalCode)).ok();
   await t.pressKey("1 2 3 4 5 tab space tab tab enter");
 
+  await t.takeScreenshot();
   await t.expect(textExists(titleTextForPostalCode)).notOk();
 });
