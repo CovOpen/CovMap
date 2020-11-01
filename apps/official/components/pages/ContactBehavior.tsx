5b9ce02413149683079e8f58ff4b5dc18dbaba16
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-11-01 23:56:09 +0100

    Update ContactBehavior.tsx

--- apps/official/components/pages/ContactBehavior.tsx
+++ apps/official/components/pages/ContactBehavior.tsx
@@ -12,31 +12,30 @@ const ContactBehavior = () => {
       <section>
         <Typography>Was ist das Kontaktverhalten und warum ist es wichtig?</Typography>
         <Typography>
-          Das Virus wird von Mensch zu Mensch übertragen. Dabei ist der{" "}
+          Das Virus wird von Mensch zu Mensch übertragen. Indem wir Kontakte reduzieren, können wir es dem Virus erschweren, sich in der Bevölkerung zu verbreiten. Dabei ist der{" "}
           <a href="https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Projekte_RKI/R-Wert-Erlaeuterung.pdf?__blob=publicationFile">
             R-Wert
           </a>{" "}
-          ist eine wichtige Maßzahl zur Steuerung der Corona Maßnahmen. Der R-Wert besagt, wie viele Personen von einer
+          ist eine wichtige Maßzahl, mit der wir beurteilen können, ob wir es dem Virus schwer genug gemacht haben. Der R-Wert besagt, wie viele Personen von einer
           infizierten Person im Durchschnitt angesteckt werden. Wenn der R-Wert 1 beträgt, bedeutet dies, dass eine
-          infizierte Person eine weitere ansteckt. Ziel ist es, durch Maßnahmen einen R Wert unter 1 zu erreichen, d.h.,
+          infizierte Person im Schnitt nur eine weitere ansteckt. Ziel ist es, durch Maßnahmen einen R Wert unter 1 zu erreichen, d.h.,
           dass eine infizierte Person im Durchschnitt weniger als eine weitere Person ansteckt. Ein R-Wert oberhalb von
           1 führt dagegen zu einer Kettenreaktion mit einem unkontrollierten Anstieg der Fallzahlen. Der R-Wert hängt
           einerseits von der Biologie des Virus ab ("wie ansteckungsfähig ist das Virus?"), andererseits von unserem
-          Kontaktverhalten ("wie viele Personen treffe ich?"). Während wir die Biologie des Virus nicht ändern können,
-          haben wir auf unser Kontaktverhalten einen großen Einfluss. Indem wir unsere Kontakte reduzieren, können wir
+          Kontaktverhalten ("auf wie viele Personen treffe ich?"). Während wir die Biologie des Virus nicht ändern können,
+          können wir durch unser Kontaktverhalten einen großen Einfluss auf das Infektionsgeschehen nehmen. Indem wir unsere Kontakte reduzieren, können wir
           es auch einem hoch ansteckenden Virus so schwer machen, eine weitere Person zu infizieren, dass der R-Wert
-          unter eins sinkt. Das Problem des R-Werts ist es, dass das Infektionsgeschehen mit mehreren Tage Verzögerung
-          abgebildet wird. Dies erscheint auch logisch, wenn man bedenkt, dass man nach einer Ansteckung mit dem
-          Coronavirus nicht sofort Symptome entwickelt. Falls sich eine Person einem Labortest unterzieht, dauert es in
-          der Regel mehrere Tage, bis das Ergebnis dem Robert-Koch-Institut mitgeteilt wird. Der R-Wert ist somit immer
+          unter 1 sinkt. Das Problem des R-Werts ist es, dass er das Infektionsgeschehen mit mehreren Tage Verzögerung
+          abbildet, da er aus der Zahl der bestätigten Infektionen berechnet wird. Dies erscheint auch logisch, wenn man bedenkt, dass man nach einer Ansteckung mit dem
+          Coronavirus nicht sofort Symptome entwickelt und auch das Ergebnis eines Labortests mit mehreren Tagen Verzögerung dem Robert-Koch-Institut mitgeteilt wird. Der R-Wert ist somit immer
           ein Blick in die Vergangenheit. Unsere Gruppe hat ein{" "}
           <a href="https://www.medrxiv.org/content/10.1101/2020.10.02.20188136v2">
             Modell für das Kontaktverhalten auf Basis von GPS Daten
           </a>{" "}
-          entwickelt. Über die Berechnung eines sogenannten Kontakt-Index können wir direkt den R-Wert berechnen. Der
+          entwickelt. Über die Berechnung eines sogenannten Kontakt-Index können wir den R-Wert vorhersagen. Der
           Vorteil vom Kontakt-Index ist nun, dass er fast ohne Verzögerung berechnet werden kann. Von einem erhöhten
           Kontaktverhalten sprechen wir, wenn unser Modell voraussagt, dass in einer Region so viele Kontakte vorhanden
-          sind, dass sich das Virus leicht ausbreiten kann und der R-Wert über 1 gehen müsste.
+          sind, dass sich das Virus leicht ausbreiten kann und wir davon ausgehen, dass der R-Wert über 1 liegen wird.
         </Typography>
       </section>
     </main>
