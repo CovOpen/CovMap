0c331b8398cc5730e107202f9423c28d227651c4
Janna Wieneke <janna.wieneke@posteo.de>
2020-11-05 11:07:10 +0100

    Move texts for credits to translation file

--- apps/official/components/pages/Credits.tsx
+++ apps/official/components/pages/Credits.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { Grid, makeStyles } from "@material-ui/core";
 import { NavigationTitle } from "app-config/components/NavigationTitle";
+import { useTranslation } from "react-i18next";
 const contributors = require("json-loader!./../../../../.all-contributorsrc");
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,12 +40,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 export const Credits = () => {
   const classes = useStyles();
+  const { t } = useTranslation("translation");
 
   return (
     <>
       <main className="sections">
         <section>
-          <NavigationTitle title={"Über die CovMap"} />
+          <NavigationTitle title={t("pages.about")} />
 
           <div className={(classes.root, classes.logobar)}>
             <Grid container spacing={3}>
@@ -59,58 +61,41 @@ export const Credits = () => {
               </Grid>
             </Grid>
           </div>
-          <Typography variant="body1">
-            CovMap ist ein Gemeinschaftsprojekt der Charité - Universitätsmedizin Berlin, des Hasso-Plattner-Instituts
-            (HPI) und der Firma NET CHECK.
-          </Typography>
-          <Typography variant="h2">Projektleitung:</Typography>
-          <Typography variant="body1">
-            Charité: Dr. med. Alexander H. Thieme, M. Sc. <br />
-            Gesamtkonzept, CovMap, Datenanalyse Symptome
-            <br />
-            <br />
-            HPI: Prof. Dr. Christoph Lippert
-            <br />
-            Datenanalyse, Modellerstellung, Prognosen
-            <br />
-            <br />
-            NET CHECK: PD. Dr. Sten Rüdiger <br />
-            Datenanalyse GPS-Daten, Kontakt-Index
+          <Typography variant="body1">{t("credits.project-description")}</Typography>
+          <Typography variant="h2">{t("credits.project-management.title")}:</Typography>
+          <Typography variant="body1" style={{ whiteSpace: "break-spaces" }}>
+            {t("credits.project-management.text")}
           </Typography>
           <Typography variant="h2">
             <br />
-            Die CovMap App wurde mit der Unterstützung von zahlreichen Personen und Unternehmen entwickelt. Wir möchten
-            an dieser Stelle unseren großen Dank aussprechen.
+            {t("credits.appreciation.title")}
             <br />
           </Typography>
-          <Typography variant="body1">Wir danken den Unternehmen:</Typography>
+          <Typography variant="body1">{t("credits.appreciation.intro")}:</Typography>
           <Grid container spacing={3}>
             <Grid item xs={12} sm={3} className={classes.logo_container}>
               <img className={classes.logo_small} src="images/credits/portbluesky.png"></img>
             </Grid>
             <Grid item xs={12} sm={9}>
-              <Typography variant="body1">
-                Port Blue Sky: für die technische Unterstüzung der App durch Ihre Mitarbeiter sowie das Organisieren von
-                weiterer Unterstützung aus der Node.js Community.
-              </Typography>
+              <Typography variant="body1">{t("credits.appreciation.port-blue-sky")}</Typography>
             </Grid>
             <Grid item xs={12} sm={3} className={classes.logo_container}>
               <img className={classes.logo_small} src="images/credits/pwc.png"></img>
             </Grid>
             <Grid item xs={12} sm={9}>
-              <Typography variant="body1">PWC: für Lob, Kritik und Rat bei der Umsetzung der CovMap.</Typography>
+              <Typography variant="body1">{t("credits.appreciation.pwc")}</Typography>
             </Grid>
             <Grid item xs={12} sm={3} className={classes.logo_container}>
               <img className={classes.logo_small} src="images/credits/maptiler.png"></img>
             </Grid>
             <Grid item xs={12} sm={9}>
-              <Typography variant="body1">Maptiler: für die Unterstützung in Form von Kartendaten.</Typography>
+              <Typography variant="body1">{t("credits.appreciation.maptiler")}</Typography>
             </Grid>
           </Grid>
         </section>
         <section>
           <Typography variant="body1">
-            Ganz besonders möchten wir folgenden Personen danken:
+            {t("credits.appreciation.contributors-intro")}:
             <br />
           </Typography>
           <Typography variant="body1">
