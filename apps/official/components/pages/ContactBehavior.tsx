3aa0b144f156cf792568924754eaf52da023e1b7
Janna Wieneke <janna.wieneke@posteo.de>
2020-11-05 10:47:35 +0100

    Set rel to noopener

--- apps/official/components/pages/ContactBehavior.tsx
+++ apps/official/components/pages/ContactBehavior.tsx
@@ -17,7 +17,7 @@ const ContactBehavior = () => {
         <Typography>
           <Trans i18nKey="contact-behavior.text-1">
             part-0
-            <a href={t("contact-behavior.external-explanation-reproduction-rate")} target="_blank">
+            <a href={t("contact-behavior.external-explanation-reproduction-rate")} target="_blank" rel="noopener">
               link-title
             </a>
             part-2
@@ -28,7 +28,7 @@ const ContactBehavior = () => {
         <Typography>
           <Trans i18nKey="contact-behavior.text-4">
             part-0
-            <a href="https://www.medrxiv.org/content/10.1101/2020.10.02.20188136v2" target="_blank">
+            <a href="https://www.medrxiv.org/content/10.1101/2020.10.02.20188136v2" target="_blank" rel="noopener">
               Modell für das Kontaktverhalten auf Basis von GPS Daten
             </a>
             part-2
